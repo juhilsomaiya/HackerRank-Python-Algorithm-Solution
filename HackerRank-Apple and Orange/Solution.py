19eea1fe0b90686729dc9e1e5d34ba74509082a2
SHAHSHIVAM594 <40867350+SHAHSHIVAM594@users.noreply.github.com>
2019-10-21 10:28:23 +0530

    Removed unnecessory Imports to improve speed.this python libraries are not using in program

--- HackerRank-Apple and Orange/Solution.py
+++ HackerRank-Apple and Orange/Solution.py
@@ -1,10 +1,6 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 # Complete the countApplesAndOranges function below.
 def countApplesAndOranges(s, t, a, b, apples, oranges):
